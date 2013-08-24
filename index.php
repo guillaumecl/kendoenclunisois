96572404b81d491311ded3763cfb235e5230af82
Guillaume Clément <gclement@baobob.org>
2013-08-24 15:42:47 +0200

    Ajout des fichiers de base

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php defined('_JEXEC') or die;?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+  <head>
+	<jdoc:include type="head" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/general.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" type="text/css" />
+	<link rel="shortcut icon" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/favicon.ico" type="image/x-icon" />
+  </head>
+  <body>
+	<div id="cadre_exterieur">
+	  <div id="logo"></div>
+	  <div id="chemin"><jdoc:include type="modules" name="chemin" style="xhtml" /></div>
+	  <div id="recherche"><jdoc:include type="modules" name="recherche" style="xhtml" /></div>
+	  <div class="clr"></div>
+	  <div id="menu_haut"><jdoc:include type="modules" name="menu_haut" style="xhtml" /></div>
+	  <div class="clr"></div>
+	  <div id="cadre_texte"><jdoc:include type="component" style="xhtml" /></div>
+	  <div class="clr"></div>
+	  <div id="pied_de_page"><jdoc:include type="modules" name="footer" style="xhtml" /></div>
+	</div>
+  </body>
